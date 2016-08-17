fe5214ff3037b9679f3beb832476504e6aa71296
Ran Cheng <neonasuka@gmail.com>
2016-08-16 20:08:21 -0400

    Update multi-taskGaussianRegression.py

--- multi-taskGaussianRegression.py
+++ multi-taskGaussianRegression.py
@@ -1,10 +1,10 @@
+import numpy as np
 
 # Import the Necessary Modules
 # GPRegression module
-from gpregressor import *
-# Kernal Module?
-# Abalone Processing module
-import abalone_data
+from numpy import gpregressor
+from numpy import abalone_data
+ 
 # Plotting modules
 import matplotlib
 from pylab import plot, show, ylim, yticks
@@ -35,29 +35,3 @@ x = cooked[1]
 num_partions = 5
 # Initialize the regressor
 gpr = GPRegressor(x,y,num_partions)
-
-# Test Values
-sigma = [0.01, 0.03, 0.1, 0.3, 1., 3.]
-Lambda = gpr.return_quantiles()
-results=np.empty((len(sigma),len(Lambda),2))
-
-# Run Tests
-output = open('results.csv','w')
-
-# Keep the same lambda across different sigmas, to save kernal computation
-for v,l in enumerate(Lambda):
-    gpr.reset_lambda(l)
-    for u,s in enumerate(sigma):
-        avg_v_error = 0
-        avg_t_error = 0
-        for i in range(0,num_partions):
-            v_err,t_err = gpr.estimate_mean(i,s,l)
-            avg_v_error += v_err
-            avg_t_error += t_err
-        avg_v_error /= num_partions
-        avg_t_error /= num_partions
-        outstring = 's: {}, l: {}, verr: {}, terr: {}\n'.format(s,l,avg_v_error,avg_t_error)
-        print outstring
-        output.write(outstring)
-
-output.close()
